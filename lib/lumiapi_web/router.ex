7b03acb37b2889c5d020b10a53f0b882820e67f8
Tan Yuanhong <leotan.yh@gmail.com>
2019-05-06 17:11:59 +0800

    test mode

--- lib/lumiapi_web/router.ex
+++ lib/lumiapi_web/router.ex
@@ -23,11 +23,12 @@ defmodule LumiapiWeb.Router do
   scope "/api/v1", LumiapiWeb do
     pipe_through :api
 
-    # get "/username/:username/password/:password/path/:path", UserController, :index
     get "/username/:username/password/:password/profile", UserController, :profile
     get "/username/:username/password/:password/current_term", UserController, :current_term
     get "/username/:username/password/:password/modules", UserController, :modules
     get "/username/:username/password/:password/module", UserController, :modules
     get "/username/:username/password/:password/announcements/:id", UserController, :announcements
+
+    get "/test/:path", UserController, :test
   end
 end
